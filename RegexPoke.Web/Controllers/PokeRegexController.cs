471b7dcfbfd21fba3c6d1890fc9508cc3b6e0616
Daniel Bottiglieri <danbott1@gmail.com>
2020-10-31 11:53:38 +0000

    Initial commit.

---
+++ RegexPoke.Web/Controllers/PokeRegexController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RegexPoke.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokeRegexController : ControllerBase
+    {
+        private readonly ILogger<PokeRegexController> _logger;
+
+        public PokeRegexController(ILogger<PokeRegexController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public string Post([FromBody] string[] regex)
+        {
+            return RegexPoke.RegexPokeService.RegexResults(regex);
+        }
+
+        [HttpGet("demo")]
+        public string GetDemo()
+        {
+            return RegexPoke.RegexPokeService.RegexResults(null);
+        }
+    }
+}
---
+++ RegexPoke/RegexPokeService.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace RegexPoke
+{
+    public class RegexPokeService
+    {
+        public static string RegexResults(string[] args)
+        {
+            var names = GetAllPokemonNames();           
+            var regexes = new List<Tuple<Regex, List<string>>>();
+            var returnString = string.Empty;
+
+            if (args != null && args.Any())
+                foreach (var arg in args)
+                {
+                    regexes.Add(new Tuple<Regex, List<string>>(new Regex(arg.ToLower()), new List<string>()));
+                }
+            else
+            {
+                regexes.Add(new Tuple<Regex, List<string>>(new Regex("(^[wi].+f$)"), new List<string>()));
+            }
+
+            foreach (var name in names)
+            {
+                foreach (var regex in regexes)
+                {
+                    if (regex.Item1.IsMatch(name))
+                        regex.Item2.Add(name);
+                }
+            }
+
+            foreach (var regex in regexes)
+            {
+                returnString += $"{regex.Item1}: [{string.Join(',', regex.Item2)}]\n";
+            }
+
+            return returnString;
+        }
+
+        public static IEnumerable<string> GetAllPokemonNames()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using Stream stream = assembly.GetManifestResourceStream(@"RegexPoke.pokemonnames.json");
+            using StreamReader r = new StreamReader(stream);
+            string json = r.ReadToEnd();
+            return JsonConvert.DeserializeObject<IEnumerable<string>>(json);
+        }
+    }
+
+}
